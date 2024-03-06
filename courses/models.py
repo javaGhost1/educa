2f6d53f773b9bdd5ac0ee6c85f7632ba62ce2a99
JavaGhost1 <Secret09!>
2024-03-06 07:59:32 +0300

    setting up the e-learning project

---
+++ courses/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self) -> str:
+        return self.title
+    
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name='course_created', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name="courses", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+class Module(models.Model):
+    Course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='modules', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
